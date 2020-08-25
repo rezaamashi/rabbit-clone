4b98a02cb17bf2272eb30043fbbf173e2b3a1790
Reza A'masyi <mnurrreza@gmail.com>
2020-08-25 23:22:30 +0700

    problem around the mikroorm and the postgresql

---
+++ dist/mikro-orm.config.js
@@ -0,0 +1,30 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const Post_1 = require("./entities/Post");
+const constants_1 = require("./constants");
+const core_1 = require("@mikro-orm/core");
+const path_1 = __importDefault(require("path"));
+console.log("dirname: ", __dirname);
+exports.default = {
+    migrations
+};
+{
+    path: path_1.default.join(__dirname, "./migrations"),
+        pattern;
+    /^[\w-]+\d+\.[tj]s$/;
+}
+;
+entities: [Post_1.Post],
+    dbName;
+'rebbit',
+    type;
+'postgresql',
+    debug;
+!constants_1.__prod__,
+;
+as;
+Parameters < typeof core_1.MikroORM.init > [0];
+//# sourceMappingURL=mikro-orm.config.js.map
