a4af07e80fdcb8f57b35eff896bf9371ca215839
Reza A'masyi <mnurrreza@gmail.com>
2021-03-19 13:28:27 +0700

    Fixing argon2 problem in user.ts (check NOTES.org)

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -8,7 +8,7 @@ import {
   ObjectType,
 } from "type-graphql";
 import { MyContext } from "../types";
-import { User } from "src/entities/User";
+import { User } from "../entities/User";
 import argon2 from "argon2";
 
 @InputType()
