d9b0b80035ea08bbc17c27ef30df349dac79df74
Reza A'masyi <mnurrreza@gmail.com>
2021-03-20 15:28:07 +0700

    Adding formik

--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -1,43 +1,18 @@
 import React from "react";
+import {Formik} from "formik";
 
 interface registerProps {}
 
-    <Formik
-      initialValues={{ name: "Sasuke" }}
-      onSubmit={(values, actions) => {
-        setTimeout(() => {
-          alert(JSON.stringify(values, null, 2));
-          actions.setSubmitting(false);
-        }, 1000);
-      }}
-    >
-      {(props) => (
-        <Form>
-          <Field name="name" validate={validateName}>
-            {({ field, form }) => (
-              <FormControl isInvalid={form.errors.name && form.touched.name}>
-                <FormLabel htmlFor="name">First name</FormLabel>
-                <Input {...field} id="name" placeholder="name" />
-                <FormErrorMessage>{form.errors.name}</FormErrorMessage>
-              </FormControl>
-            )}
-          </Field>
-          <Button
-            mt={4}
-            colorScheme="teal"
-            isLoading={props.isSubmitting}
-            type="submit"
-          >
-            Submit
-          </Button>
-        </Form>
-      )}
-    </Formik>
-  );
-}
-
 const Register: React.FC<registerProps> = ({}) => {
-  return <div>register page</div>;
+  return (  
+<Formik>
+      {() => (
+<form>
+					<div>
+						hello
+	</div>
+	  </form>      )}
+</Formik>	 );
 };
 
 export default Register;
