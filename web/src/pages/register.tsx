31ded2b46ce6708319bd0bef3e385157d937f48d
Reza A'masyi <mnurrreza@gmail.com>
2021-03-20 15:14:01 +0700

    Moving to simpler file structure

---
+++ web/src/pages/register.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+interface registerProps {}
+
+    <Formik
+      initialValues={{ name: "Sasuke" }}
+      onSubmit={(values, actions) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          actions.setSubmitting(false);
+        }, 1000);
+      }}
+    >
+      {(props) => (
+        <Form>
+          <Field name="name" validate={validateName}>
+            {({ field, form }) => (
+              <FormControl isInvalid={form.errors.name && form.touched.name}>
+                <FormLabel htmlFor="name">First name</FormLabel>
+                <Input {...field} id="name" placeholder="name" />
+                <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+              </FormControl>
+            )}
+          </Field>
+          <Button
+            mt={4}
+            colorScheme="teal"
+            isLoading={props.isSubmitting}
+            type="submit"
+          >
+            Submit
+          </Button>
+        </Form>
+      )}
+    </Formik>
+  );
+}
+
+const Register: React.FC<registerProps> = ({}) => {
+  return <div>register page</div>;
+};
+
+export default Register;
