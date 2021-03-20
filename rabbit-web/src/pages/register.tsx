a41f68c1ca85d1c4657f5184f94e102b9c32a2fb
Reza A'masyi <mnurrreza@gmail.com>
2021-03-20 14:54:24 +0700

    Creating different folder for server and web side

---
+++ rabbit-web/src/pages/register.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+interface registerProps {
+
+}
+
+ const Register: React.FC<registerProps> = ({}) => {
+   return <div>register page</div>;
+}
+
+export default Register
+
