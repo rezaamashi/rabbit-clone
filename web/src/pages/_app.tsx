81193ce3b29586bb410136a3a994c8649015e970
Reza A'masyi <mnurrreza@gmail.com>
2021-03-20 21:40:03 +0700

    Fix caching problem and redirection

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,12 +1,65 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import { createClient, Provider } from "urql";
+import { cacheExchange, Cache, QueryInput } from "@urql/exchange-graphcache";
+import { createClient, dedupExchange, fetchExchange, Provider } from "urql";
+import { MeDocument, MeQuery } from "../generated/graphql";
 import theme from "../theme";
+
+function betterUpdateQuery(
+  cache: Cache,
+  qi: QueryInput,
+  result: any,
+  fn: (r: Result, q: Query) => Query
+) {
+  return cache.updateQuery(qi, (data) => fn(result, data as any) as any);
+}
 
 const client = createClient({
   url: "http://localhost:5000/graphql",
   fetchOptions: {
     credentials: "include",
   },
+  exchanges: [
+    dedupExchange,
+    cacheExchange({
+      updates: {
+        Mutation: {
+          login: (_result, args, cache, info) => {
+            betterUpdateQuery<LoginMutation, MeQuery>(
+	      cache,
+	      { query: MeDocument, }
+	      _result,
+              (result, query) => {
+		if (result.login.errors) {
+		  return query;
+		} else {
+		  return {
+		    me: result.login.user,
+		  };
+		}
+	      }
+	    );
+          },
+          register: (_result, args, cache, info) => {
+            betterUpdateQuery<RegisterMutation, MeQuery>(
+	      cache,
+	      { query: MeDocument, }
+	      _result,
+              (result, query) => {
+		if (result.register.errors) {
+		  return query;
+		} else {
+		  return {
+		    me: result.register.user,
+		  };
+		}
+	      }
+	    );
+          },
+        },
+      },
+    }),
+    fetchExchange,
+  ],
 });
 
 function MyApp({ Component, pageProps }: any) {
