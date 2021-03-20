a7f8cdbdecda29ece64c6544bb4d45d9f4433696
Reza A'masyi <mnurrreza@gmail.com>
2021-03-20 19:58:22 +0700

    Adding EntityManager directly to user resolver

--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -11,17 +11,26 @@ import React from "react";
 import { InputField } from "../components/InputField";
 import { Wrapper } from "../components/Wrapper";
 import { useRegisterMutation } from "../generated/graphql";
+import { toErrorMap } from "../utils/toErrorMap";
+import { useRouter } from "next/router";
 
 interface registerProps {}
 
 const Register: React.FC<registerProps> = ({}) => {
+  const router = useRouter();
   const [, register] = useRegisterMutation();
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: "", password: "" }}
-        onSubmit={async (values) => {
+        onSubmit={async (values, { setErrors }) => {
           const response = await register(values);
+          if (response.data?.register.errors) {
+            setErrors(toErrorMap(response.data.register.errors));
+          } else if (response.data?.register.user) {
+            //worked
+            router.push("/");
+          }
         }}
       >
         {({ isSubmitting }) => (
@@ -41,7 +50,7 @@ const Register: React.FC<registerProps> = ({}) => {
             </Box>
             <Button
               mt={4}
-              ColorScheme="teal"
+              colorScheme="teal"
               isLoading={isSubmitting}
               type="submit"
             >
